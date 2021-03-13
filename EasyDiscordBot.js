892268bc0e19ef3be9c384e5bf6b7c9b1775f19f
GRZ4NA <grzanatv@gmail.com>
2021-03-13 01:16:27 +0100

    Structure rework + help command + createEmbed

--- EasyDiscordBot.js
+++ EasyDiscordBot.js
@@ -1,50 +1,76 @@
-import { Client } from "discord.js";
+import "discord.js";
+import { Client, MessageEmbed } from "discord.js";
 import http from 'http';
 
 class EasyDiscordBot {
     constructor(params) {
         try {
-            this.client = new Client();
+            this.bot = {
+                client: new Client(),
+                name: params.name.toString(),
+                version: params.version ? params.version.toString() : "1.0.0"
+            };
+            this.config = {
+                discordToken: params.discordToken.toString(),
+                prefix: params.prefix ? params.prefix.toString() : () => { console.warn('Prefix has not been specified. Using default prefix (!)'); return "!"; },
+                botMessageDeleteTimeout: 5000,
+                accentColor: "#000",
+                responseTable: {
+                    commandNotFound: 'The command [command] does not exist.',
+                    insufficientPermissions: "You don't have the required permissions to use this command.",
+                    userNotConnected: "In order to use this command you have to be connected to a voice channel."
+                }
+            };
             this.commandsList = [
                 {
                     name: 'version',
                     description: "This command displays your bot's version.",
                     permissions: 0,
                     exec: async m => {
-                        await m.reply(`${this.name} version ${this.version}`);
+                        await m.reply(`${this.bot.name} version ${this.bot.version}`);
+                    }
+                },
+                {
+                    name: 'help',
+                    description: "Displays list of available commands",
+                    permissions: 0,
+                    exec: async m => {
+                        const message = EasyDiscordBot.createEmbed({
+                            title: `Help for ${this.bot.name}`,
+                            color: this.config.accentColor,
+                            description: 'List of available commands',
+                            footer: `${this.bot.name} ${this.bot.version}`,
+                            fields: () => {
+                                const commands = [];
+                                for(let i = 0; i < this.commandsList.length; i++) {
+                                    const commandObj = { inline: false };
+                                    commandObj.title = this.commandsList[i].name;
+                                    commandObj.value = this.commandsList[i].description;
+                                    commands.push(commandObj);
+                                }
+                                return commands;
+                            }
+                        });
+                        await m.channel.send(message);
                     }
                 }
             ];
-            this.responseTable = {
-                commandNotFound: 'The command [command] does not exist.',
-                insufficientPermissions: "You don't have the required permissions to use this command.",
-                userNotConnected: "In order to use this command you have to be connected to a voice channel."
-            }
-            this.name = params.name;
-            this.version = params.version ? params.version : "1.0.0";
-            this.discordToken = params.discordToken;
-            if(params.prefix) {
-                this.prefix = params.prefix.toString();
-            }
-            else {
-                console.warn('Prefix has not been specified. Using default prefix (!)');
-                this.prefix = '!';
-            }
-
-            this.onReady = () => {
-                console.log('Connected!');
-                console.log(' ');
-                return;
-            }
-            this.onMessage = message => {
-                console.log(`${message.author.username} (${message.channel.name}): ${message.content}`);
-            };
-            this.onCommand = command => {
-                console.log(`${command.author.username} (${command.channel.name}): ${command.content}`);
-                console.log(command.command);
-            }
-            this.onError = error => {
-                console.error(error);
+            this.events = {
+                onReady: () => {
+                    console.log('Connected!');
+                    console.log(' ');
+                    return;
+                },
+                onMessage: message => {
+                    console.log(`${message.author.username} (${message.channel.name}): ${message.content}`);
+                },
+                onCommand: command => {
+                    console.log(`${command.author.username} (${command.channel.name}): ${command.content}`);
+                    console.log(command.command);
+                },
+                onError: error => {
+                    console.error(error);
+                }
             };
         }
         catch (e) {
@@ -54,53 +80,54 @@ class EasyDiscordBot {
         }
     }
     async start(port) {
-        console.log(`Bot name: ${this.name}`);
-        console.log(`Bot version: ${this.version}`);
-        console.log(`Bot prefix: ${this.prefix}`);
+        console.log(`Bot name: ${this.bot.name}`);
+        console.log(`Bot version: ${this.bot.version}`);
+        console.log(`Bot prefix: ${this.config.prefix}`);
         console.log('Based on discord.js library & EasyDiscordBot wrapper created by GRZANA (https://github.com/GRZ4NA)');
         console.log(' ');
         try {
             if(port) {
                 console.log('A port has been specified. Creating http server...');
-                http.createServer().listen(port);
-                console.log(`Listening on port ${port}`);
+                this.config.port = port;
+                http.createServer().listen(this.config.port);
+                console.log(`Listening on port ${this.config.port}`);
             }
             console.log('Connecting to Discord...');
-            this.client.on('ready', () =>{
-                this.onReady();
+            this.bot.client.on('ready', () =>{
+                this.events.onReady();
                 return true;
             });
-            this.client.on('error', e => this.onError(e));
-            this.client.on('message', message => {
+            this.bot.client.on('error', e => this.events.onError(e));
+            this.bot.client.on('message', message => {
                 message.command = {};
-                if(message.content.startsWith(this.prefix) && !message.content.replace(this.prefix, '').startsWith(this.prefix)) {
+                if(message.content.startsWith(this.config.prefix) && !message.content.replace(this.config.prefix, '').startsWith(this.config.prefix)) {
                     message.command.isCommand = true;
-                    message.command.name = message.content.replace(this.prefix, '').split(' ')[0];
-                    message.command.arguments = message.content.replace(this.prefix, '').replace(message.command.name, '').split(', ');
+                    message.command.name = message.content.replace(this.config.prefix, '').split(' ')[0];
+                    message.command.arguments = message.content.replace(this.config.prefix, '').replace(message.command.name, '').split(', ');
                 }
                 else {
                     message.command.isCommand = false;
                 }
                 if(message.command.isCommand && !message.author.bot) {
-                    this.onCommand(message);
+                    this.events.onCommand(message);
                     const command = this.getCommand(message.command.name);
                     if(command) {
                         if(this.permissionsProxy(message, command)) {
                             command.exec instanceof Function ? command.exec(message) : m => { console.error(`The exec property on ${command.name} is not a function`); return; };
                         }
                         else {
-                            message.reply(this.responseTable.insufficientPermissions);
+                            message.reply(this.config.responseTable.insufficientPermissions).then(m => { if(this.config.botMessageDeleteTimeout) { m.delete({ timeout: this.config.botMessageDeleteTimeout }) } });
                         }
                     }
                     else {
-                        message.reply(this.responseTable.commandNotFound.replace('[command]', message.command.name));
+                        message.reply(this.config.responseTable.commandNotFound.replace('[command]', message.command.name)).then(m => { if(this.config.botMessageDeleteTimeout) { m.delete({ timeout: this.config.botMessageDeleteTimeout }) } });
                     }
                 }
                 else {
-                    this.onMessage(message);
+                    this.events.onMessage(message);
                 }
             });
-            await this.client.login(this.discordToken);
+            await this.bot.client.login(this.config.discordToken);
         }
         catch(e) {
             console.error('An error occured during login procedure. If this problem persists please check your app token.');
@@ -160,6 +187,30 @@ class EasyDiscordBot {
             }
         }
         return false;
+    }
+    static createEmbed(params) {
+        try {
+            const message = new MessageEmbed()
+            message.setColor(params.color ? params.color.toString() : "#000");
+            if(params.title) { message.setTitle(params.title.toString()); }
+            if(params.url) { message.setURL(params.url.toString()); }
+            if(params.author && params.author instanceof Array) { message.setAuthor(params.author[0], params.author[1], params.author[2]); }
+            if(params.description) { message.setDescription(params.description.toString()); }
+            if(params.thumbnail) { message.setThumbnail(params.thumbnail); }
+            if(params.showTimestamp === true) { message.setTimestamp(); }
+            if(params.image) { message.setImage(params.image.toString()); }
+            if(params.footer) { message.setFooter(params.footer.toString()); }
+            if(params.fields && params.fields instanceof Array) {
+                for(let i = 0; i < params.fields.length; i++) {
+                    message.addField(params.fields[i].title, params.fields[i].value, params.fields[i].inline && params.fields[i].inline instanceof Boolean ? params.fields[i].inline : false);
+                }
+            }
+            return message;
+        }
+        catch (e) {
+            console.error(e);
+            return;
+        }
     }
 }
 
