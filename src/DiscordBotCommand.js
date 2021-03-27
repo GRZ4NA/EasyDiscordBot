ef99a42a0a6b84ed7ad36832ed188b595f60b53e
GRZ4NA <grzanatv@gmail.com>
2021-03-27 16:04:12 +0100

    Revert "Removed backwards compatibility"
    This reverts commit 60834cf13e07fb26b5d6d447af4f471a102d3c92.

--- src/DiscordBotCommand.js
+++ src/DiscordBotCommand.js
@@ -91,6 +91,12 @@ class DiscordBotCommand {
                         }
                     }
                 }
+                //BACKWARDS COMPATIBILITY
+                if(params.permissions.admin == true) {
+                    console.warn(`WARN! The "admin" property is deprecated and support for it is going to be removed in the future. Please use { permissions: ['ADMINISTRATOR'] } instead.`)
+                    this.permissions.permissions = [];
+                    this.permissions.permissions.push('ADMINISTRATOR');
+                }
 
                 //ROLES
                 if(params.permissions.roles) {
